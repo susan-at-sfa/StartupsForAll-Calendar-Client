649b1bc05514af1d2654f69701fa8649ef957042
Usarneme <jamestcoop@gmail.com>
2021-09-01 15:10:00 -0700

    :fire: add makes request to server for eventbrite info; WIP

--- src/components/AddEvent/EventbriteIDInput.tsx
+++ src/components/AddEvent/EventbriteIDInput.tsx
@@ -3,17 +3,17 @@ import RedButton from "../RedButton";
 import FormInput from "../FormInput";
 import FormLabel from "../FormLabel";
 import styled from "@emotion/styled";
-import { useAppDispatch } from "../../hooks";
 
-const EventbriteIDInput: FC = () => {
+interface EventbriteIDInputProps {
+  handleSubmit(value: string): void;
+}
+
+const EventbriteIDInput: FC<EventbriteIDInputProps> = (props) => {
   const [eventbriteID, setEventbriteID] = useState<string>("");
-
-  const dispatch = useAppDispatch();
 
   const submitForm = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const payload = { eventbriteID };
-    // dispatch(apiLookup(payload));
+    props.handleSubmit(eventbriteID);
   };
 
   return (
--- src/containers/Add/Add.tsx
+++ src/containers/Add/Add.tsx
@@ -1,14 +1,29 @@
-import { FC } from "react";
-// import RedButton from "../../components/RedButton";
-// import FormInput from "../../components/FormInput";
-// import FormLabel from "../../components/FormLabel";
-// import styled from "@emotion/styled";
-import AuthorizeToAddEvents from "../../components/AddEvent/AuthorizeToAddEvents";
+import { FC, useState } from "react";
+import { makeRequest } from "../../store/utils/makeRequest";
+import { useAppSelector } from "../../hooks";
+// import AuthorizeToAddEvents from "../../components/AddEvent/AuthorizeToAddEvents";
+import EventbriteIDInput from "../../components/AddEvent/EventbriteIDInput";
 
 const Add: FC = () => {
+  const token = useAppSelector(({ auth }) => auth.token);
+  const [eventBriteDetails, setEventBriteDetails] = useState<any>({});
   // TODO: add logic to conditionally render depending on state
-  // TODO: add state
-  return <AuthorizeToAddEvents />;
+  const handleSubmit = async (id: string) => {
+    const url = `${process.env.REACT_APP_API_URL}/events/event-brite/${id}`;
+    const method = "GET";
+    try {
+      const { success, data, error } = await makeRequest(url, method, token);
+      if (error) throw new Error(error);
+      if (success) setEventBriteDetails(data);
+      console.log(eventBriteDetails);
+    } catch (e) {
+      // TODO: handle error
+      console.error(e);
+    }
+  };
+
+  // return <AuthorizeToAddEvents />;
+  return <EventbriteIDInput handleSubmit={handleSubmit} />;
 };
 
 export default Add;
