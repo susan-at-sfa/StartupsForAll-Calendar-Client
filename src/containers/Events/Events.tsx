46eee0cd1da35b92bf0536235e85c6d3e8a4d8ea
Ryan Walker <ryandanielwalker@gmail.com>
2021-09-10 20:40:15 -0700

    Successfully add scrollable, sticky month sections and headers to list view

--- src/containers/Events/Events.tsx
+++ src/containers/Events/Events.tsx
@@ -24,8 +24,38 @@ export interface EventPageProps {
 const Events: FC = () => {
   const events = useAppSelector(({ dbEvent }) => dbEvent.dbEvents);
 
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ]
+
+  const sections = months.map((month, index) => {
+    return (
+      <MonthSection id={month} key={index}>
+        <MonthHeader>
+          <h1><span>{month}</span></h1>
+        </MonthHeader>
+        <DisplayEvents>
+
+        </DisplayEvents>
+      </MonthSection>
+    )
+  })
+
+
   return (
     <ListEventContainer>
+      <>{sections}</>
       <ul className="displayListEventList">
         {events.map((event: any) => {
           const { id, category, title, start_date, start_time, end_time, creator_name, topics } = event;
@@ -49,19 +79,45 @@ const Events: FC = () => {
 
 export default Events;
 
-const ListEventContainer = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-justify-content: center;
-.displayListEventList{
-  padding: 0;
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  flex-wrap: wrap;
-  list-style: none;
-}
+const MonthSection = styled.section`
+height: 85px;
 `
+const MonthHeader = styled.div`
+  position: sticky;;
+  top: 270px;
+  h1 {
+    color: #C79288;
+    font-style: normal;
+    font-size: 13px;
+    font-weight: bold;
+    width: 100%;
+    text-align: left;
+    border-bottom: 1px solid #C79288;
+    line-height: 2px;
+    margin: 10px 0 20px 10px;
+  }
+  span{
+    background:#fff;
+    padding:0 15px;
+  }
+  `
+const DisplayEvents = styled.div`
+  
+`
+
+const ListEventContainer = styled.div`
+   display: flex;
+   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+//   .displayListEventList{
+  //     padding: 0;
+  //   margin: 0;
+  //   display: flex;
+  //   flex-direction: column;
+  //   align-items: center;
+  //   justify-content: center;
+  //   flex-wrap: wrap;
+  //   list-style: none;
+  // }
+  `
