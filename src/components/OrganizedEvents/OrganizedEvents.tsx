21aa1cb6e34a18e0f82ac777345a1de6c933a521
Ryan Walker <ryandanielwalker@gmail.com>
2021-09-14 17:32:01 -0700

    Refactor organized events to keep empty array and conditionally render no events if .length is not true

--- src/components/OrganizedEvents/OrganizedEvents.tsx
+++ src/components/OrganizedEvents/OrganizedEvents.tsx
@@ -37,7 +37,7 @@ const OrganizedEventsComponent: FC = () => {
       if (monthsInYear.includes(String(month))) {
         return;
       }
-      organizedEvents[year][month] = [{ display: "No Events" }];
+      organizedEvents[year][month] = [];
     })
   })
 
@@ -48,31 +48,30 @@ const OrganizedEventsComponent: FC = () => {
           <React.Fragment key={year}>
             {Object.entries(months).map(([month, displayEvents]) => {
               return (
-                <MonthSection key={month} id={month}>
+                <MonthSection key={`${year}-${month}`} id={month}>
                   <MonthHeader>
                     <h1><span>{MonthObject[month]} {year}</span></h1>
                   </MonthHeader>
-                  {displayEvents.map((displayEvent: any) => {
-                    const { id, category, title, start_date, start_time, end_time, creator_name, topics } = displayEvent;
-                    if (displayEvent.display) {
-                      return (
-                        <div id="noEvents">
-                          <h1>{displayEvent.display}</h1>
-                        </div>
-                      )
-                    }
-                    return <ListEvent
-                      key={id}
-                      id={id}
-                      category={category}
-                      title={title}
-                      date={start_date}
-                      start_time={start_time}
-                      end_time={end_time}
-                      creator_name={creator_name}
-                      topics={topics}
-                    />
-                  })
+                  {displayEvents.length ? (
+                    displayEvents.map((displayEvent: any) => {
+                      const { id, category, title, start_date, start_time, end_time, creator_name, topics } = displayEvent;
+                      return <ListEvent
+                        key={id}
+                        id={id}
+                        category={category}
+                        title={title}
+                        date={start_date}
+                        start_time={start_time}
+                        end_time={end_time}
+                        creator_name={creator_name}
+                        topics={topics}
+                      />
+                    })
+                  ) : (
+                    <div id="noEvents">
+                      <h1>No Events</h1>
+                    </div>
+                  )
                   }
                 </MonthSection>
               )
