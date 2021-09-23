6dd79ca980aec5d03e3c2d4b268a537746340060
Ryan Walker <ryandanielwalker@gmail.com>
2021-09-23 11:11:45 -0700

    Update branch to match dev with addition of calendar

---
+++ src/constants/Device.ts
@@ -0,0 +1,8 @@
+const size = {
+  mobile: '320px',
+  desktop: '1024px'
+}
+export const device = {
+  mobile: `(min-width: ${size.mobile})`,
+  desktop: `(min-width: ${size.mobile})`
+}
--- src/containers/Events/Events.tsx
+++ src/containers/Events/Events.tsx
@@ -5,7 +5,7 @@ import { FiList } from "react-icons/fi";
 import { setFilterModalOpen } from "../../store/slices/filterModal/showFilterModalSlice";
 import EventsList from "../../components/EventList/EventsList";
 import GoogleCalendar from "../../components/Google/GoogleCalendar";
-import AdminGoogle from "../../components/Google/AdminGoogle";
+// import AdminGoogle from "../../components/Google/AdminGoogle";
 
 const Events: FC = () => {
   const dispatch = useAppDispatch();
@@ -25,28 +25,28 @@ const Events: FC = () => {
     //     <EventsList />
     //   </ListEventContainer>
     // </Wrapper >
-    // <Wrapper>
-    //   <div className="eventBox">
-    //     <ListEventContainer>
-    //       <FilterButton>
-    //         <div
-    //           id="filterClick"
-    //           onClick={() => dispatch(setFilterModalOpen(true))}
-    //         >
-    //           <FiList id="filterIcon" />
-    //           <p> Filters</p>
-    //         </div>
-    //       </FilterButton>
-    //       <EventsList />
-    //     </ListEventContainer>
-    //   </div>
-    //   <CalendarDiv>
-    //     <GoogleCalendar />
-    //   </CalendarDiv>
-    // </Wrapper >
     <Wrapper>
-      <AdminGoogle />
-    </Wrapper>
+      <div className="eventBox">
+        <ListEventContainer>
+          <FilterButton>
+            <div
+              id="filterClick"
+              onClick={() => dispatch(setFilterModalOpen(true))}
+            >
+              <FiList id="filterIcon" />
+              <p> Filters</p>
+            </div>
+          </FilterButton>
+          <EventsList />
+        </ListEventContainer>
+      </div>
+      <CalendarDiv>
+        <GoogleCalendar />
+      </CalendarDiv>
+    </Wrapper >
+    //   <Wrapper>
+    //     <AdminGoogle />
+    //   </Wrapper>
   );
 };
 
