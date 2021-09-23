727c08876ce74488af00f1248463810cfc5ce42d
Usarneme <jamestcoop@gmail.com>
2021-09-23 16:21:47 -0700

    :tada: fixes redux persist setup by ensuring the app is wrapped in a provider - all redux store functions appear to be working the same as before redux persist was included

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import configureStore from './store/store';
-// import { Provider } from 'react-redux';
+import configureStore from "./store/store";
+import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor } from "./store/store";
 
-// const initialState = {};
-// const store = configureStore(initialState);
+const initialState = {};
+const store = configureStore(initialState);
 
 ReactDOM.render(
   <React.StrictMode>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
