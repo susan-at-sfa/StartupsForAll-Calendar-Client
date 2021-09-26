949c51a8796d3dac717a3c159a97aa51238fb89c
Usarneme <jamestcoop@gmail.com>
2021-09-26 09:29:01 -0700

    updates db events saga to close the modal after filters are applying/db is queried

--- src/store/slices/dbEvent/sagas.ts
+++ src/store/slices/dbEvent/sagas.ts
@@ -2,12 +2,11 @@ import { put, call, takeEvery } from 'redux-saga/effects';
 import { toast } from 'react-toastify';
 import { makeRequest } from '../../utils/makeRequest';
 import { setAllDbEvents, getAllDbEvents, getDbEventsByFilter } from './dbEventSlice';
+import { setFilterModalOpen } from '../filterModal/showFilterModalSlice';
 
 const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:1323';
 
 export function* fetchAllDbEvents() {
-  /*RETURNS EVENTS IN ASC ORDER BY DATE. CURRENTLY MAKES CALL TO ordered_date ENDPOINT WHICH WILL BE CHANGING*/
-  // const { success, data, error } = yield call(makeRequest, `${BASE_URL}/events/ordered_date`, "GET");
   const { success, data, error } = yield call(makeRequest, `${BASE_URL}/events`, "GET");
 
   if (success) {
@@ -23,25 +22,25 @@ export function* fetchAllDbEvents() {
 
 export function* fetchDbEventsByFilter(dispatch: any) {
   console.log("HIT fetch db events by filter saga with filter:", dispatch);
-  // TODO: transform and send request
   let query = "?";
   if (dispatch.payload) {
     const { categories, topics } = dispatch.payload;
+    console.log("destructured topics and categories", topics, categories);
     if (categories) {
       query += "category=";
       categories.forEach((category: string) => query += category + ",");
+      // remove the extra comma
+      query = query.substring(0, query.length - 1);
     }
-    // remove the extra comma
-    query = query.substring(0, query.length - 1);
     if (categories && topics) {
       query += "&";
     }
     if (topics) {
       query += "topics=";
       topics.forEach((topic: string) => query += topic + ",");
+      // remove the extra comma
+      query = query.substring(0, query.length - 1);
     }
-    // remove the extra comma
-    query = query.substring(0, query.length - 1);
 
     const url = `${BASE_URL}/events${query}`;
     console.log("Making query:", url);
@@ -49,6 +48,7 @@ export function* fetchDbEventsByFilter(dispatch: any) {
 
     if (success) {
       yield put(setAllDbEvents(data));
+      yield put(setFilterModalOpen(false));
     }
     if (error) {
       error.json().then((errData: any) => {
